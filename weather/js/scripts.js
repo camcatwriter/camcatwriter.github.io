998b788e5c287dd9b1402e4e941c4a1e06790677
Cam Stephens Account <thetotalbookworm6000@gmail.com>
2021-04-30 23:00:50 -0600

    commented out new array idk if i need

--- weather/js/scripts.js
+++ weather/js/scripts.js
@@ -21,14 +21,14 @@ fetch(apiURL)
         const y = mydate.getDay();
         console.log(y);
 
-        const myweekday = newArray(7);
-        myweekday[0] = "Sunday";
-        myweekday[1] = "Monday";
-        myweekday[2] = "Tuesday";
-        myweekday[3] = "Wednesday";
-        myweekday[4] = "Thursday";
-        myweekday[5] = "Friday";
-        myweekday[6] = "Saturday";
+        // const myweekday = newArray(7);
+        // myweekday[0] = "Sunday";
+        // myweekday[1] = "Monday";
+        // myweekday[2] = "Tuesday";
+        // myweekday[3] = "Wednesday";
+        // myweekday[4] = "Thursday";
+        // myweekday[5] = "Friday";
+        // myweekday[6] = "Saturday";
 
 
 
