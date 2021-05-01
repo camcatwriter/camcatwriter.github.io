a55330684dbf97c8f45e9e24a381ac51975a7057
Cam Stephens Account <thetotalbookworm6000@gmail.com>
2021-04-30 21:52:28 -0600

    java edits

--- weather/js/scripts.js
+++ weather/js/scripts.js
@@ -1,7 +1,36 @@
-const apiURL = "//api.openweathermap.org/data/2.5/weather?id=5816861&appid=ed5cef8d7c6703faf7f59fa04deadba6&units=imperial"
+const apiURL = "//api.openweathermap.org/data/2.5/weather?zip=02111,us&appid=ed5cef8d7c6703faf7f59fa04deadba6&units=imperial"
 
 fetch(apiURL)
     .then((response) => response.json())
     .then((weatherInfo) => {
-        console.log(changeme);
-    })
+        console.log(weatherInfo);
+
+        document.getElementById("city").innerHTML=weatherInfo.name;
+        document.getElementById("currentTemp").innerHTML=weatherInfo.main.temp;
+        document.getElementById("windSpeed").innerHTML=weatherInfo.wind.speed;
+
+        const iconcode = weatherInfo.weather[0].icon;
+        console.log(iconcode);
+
+        const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
+        console.log(icon_path);
+
+        document.getElementById('weather_icon').src = icon_path;
+
+        const mydate = new Date();
+        const y = mydate.getDay();
+        console.log(y);
+
+        const myweekday = newArray(7);
+        myweekday[0] = "Sunday";
+        myweekday[1] = "Monday";
+        myweekday[2] = "Tuesday";
+        myweekday[3] = "Wednesday";
+        myweekday[4] = "Thursday";
+        myweekday[5] = "Friday";
+        myweekday[6] = "Saturday";
+
+        
+
+
+    });
