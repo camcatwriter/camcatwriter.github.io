2ca1affa2b5d734e6411b257468431d9ac86d8cc
Cam Stephens Account <thetotalbookworm6000@gmail.com>
2021-05-01 12:19:44 -0600

    alt

--- weather/js/scripts.js
+++ weather/js/scripts.js
@@ -57,6 +57,7 @@ fetch(apiURL)
             let iconPath = "//openweathermap.org/img/w/" + iconcode + ".png";
             let theIcon = document.createElement("img")
             theIcon.src = iconPath;
+            theIcon.alt = weatherInfo.list[i].weather[0].descrtiption;
 
             let theDay = document.createElement("div");
             theDay.appendChild(theDayName);
